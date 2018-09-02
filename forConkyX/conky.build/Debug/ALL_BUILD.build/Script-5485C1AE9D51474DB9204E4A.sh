ca5d2c9d35a5e66047d42b4d8802da792e24eec5
npyl <n.pylarinos@hotmail.com>
2018-09-02 12:36:58 +0300

    stuff

--- forConkyX/conky.build/Debug/ALL_BUILD.build/Script-5485C1AE9D51474DB9204E4A.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/npyl/Manage-Conky/ConkyX/conky-for-macOS/forConkyX -f /Users/npyl/Manage-Conky/ConkyX/conky-for-macOS/forConkyX/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- forConkyX/src/conky.build/Debug/conky.build/Objects-normal/x86_64/c++wrap.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/npyl/Manage-Conky/ConkyX/conky-for-macOS/src/c++wrap.cc \
-  /Users/npyl/Manage-Conky/ConkyX/conky-for-macOS/forConkyX/config.h \
-  /Users/npyl/Manage-Conky/ConkyX/conky-for-macOS/src/c++wrap.hh
